e912b97ef08ab3ebc6e4e3376dd71011e3a8176d
Santiago Andrés Ardiles Páez <santiagoandres@outlook.de>
2020-12-07 12:39:02 +0100

    Third project; Reviews.

---
+++ react-projects/3-reviews/src/index.js
@@ -0,0 +1,16 @@
+/*    index.js    */
+
+// Setup.
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Styles.
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
