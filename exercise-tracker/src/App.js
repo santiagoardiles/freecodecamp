ccea386ab0439e4541d199a93daf3e43e4f8517c
Santiago Andrés Ardiles Páez <santiagoandres@outlook.de>
2020-12-27 16:03:52 +0100

    Create exercise component.

--- exercise-tracker/src/App.js
+++ exercise-tracker/src/App.js
@@ -17,12 +17,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <Router>
-      <Navbar />
-      <br />
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
   );
 }
--- exercise-tracker/src/components/create-exercise.component.js
+++ exercise-tracker/src/components/create-exercise.component.js
@@ -1,10 +1,117 @@
 /*       Create exercise       */
 
 // Setup.
-import React from "react";
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+
+// Styles.
+import "react-datepicker/dist/react-datepicker.css";
 
 function CreateExercise() {
-  return <div>Create Exercise</div>;
+  // Hooks.
+  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
+  const [duration, setDuration] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
+
+  // componentDidMount.
+  useEffect(() => {
+    setUsers(["Joey", "Mary"]);
+  }, []);
+
+  // `onChange` handlers.
+  const handleChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleChangeDuration = (e) => {
+    setDuration(e.target.value);
+  };
+
+  const handleChangeDate = (date) => {
+    setDate(date);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const exercise = {
+      username,
+      description,
+      duration,
+      date,
+    };
+
+    console.log(exercise);
+
+    //window.location = "/";
+  };
+
+  return (
+    <div>
+      <h3>Create New Exercise Log</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select
+            required
+            className="form-control"
+            value={username}
+            onChange={handleChangeUsername}
+          >
+            {users.map((user) => {
+              return (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label>Description: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={description}
+            onChange={handleChangeDescription}
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input
+            type="text"
+            className="form-control"
+            value={duration}
+            onChange={handleChangeDuration}
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker selected={date} onChange={handleChangeDate} />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create Exercise Log"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CreateExercise;
