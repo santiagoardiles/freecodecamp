c8ab8afc6a8073b9784a6d6063a46ee1ed354f38
Santiago Andrés Ardiles Páez <santiagoandres@outlook.de>
2020-12-27 18:44:48 +0100

    Finishing.

--- exercise-tracker/src/components/edit-exercise.component.js
+++ exercise-tracker/src/components/edit-exercise.component.js
@@ -1,10 +1,142 @@
-/*       Edit exercise       */
+/*       Edit exercise.       */
 
 // Setup.
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
 
-function EditExercise() {
-  return <div>Edit Exercise</div>;
+// Styles.
+import "react-datepicker/dist/react-datepicker.css";
+
+function EditExercise(props) {
+  // Hooks.
+  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
+  const [duration, setDuration] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
+
+  // componentDidMount.
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/exercises/${props.match.params.id}`)
+      .then((res) => {
+        setUsername(res.data.username);
+        setDescription(res.data.description);
+        setDuration(res.data.duration);
+        setDate(new Date(res.data.date));
+      });
+
+    axios.get("http://localhost:5000/users/").then((res) => {
+      if (res.data.length > 0) {
+        setUsers(res.data.map((user) => user.username));
+      }
+    });
+  }, []);
+
+  // Handlers.
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleDuration = (e) => {
+    setDuration(e.target.value);
+  };
+
+  const handleDate = (date) => {
+    setDate(date);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const exercise = {
+      username,
+      description,
+      duration,
+      date,
+    };
+
+    axios
+      .post(
+        `http://localhost:5000/exercises/update/${props.match.params.id}`,
+        exercise
+      )
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+
+    window.location = "/";
+  };
+
+  return (
+    <div>
+      <h3>Edit Exercise Log</h3>
+      <form onSubmit={handleSubmit}>
+        {/* Username's dropdown menu. */}
+        <div className="form-group">
+          <label>Username: </label>
+          <select
+            required
+            className="form-control"
+            value={username}
+            onChange={handleUsername}
+          >
+            {users.map((user) => {
+              return (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+
+        {/* Description field. */}
+        <div className="form-group">
+          <label>Description: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={description}
+            onChange={handleDescription}
+          />
+        </div>
+
+        {/* Duration field. */}
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input
+            type="text"
+            className="form-control"
+            value={duration}
+            onChange={handleDuration}
+          />
+        </div>
+
+        {/* Date field. */}
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker selected={date} onChange={handleDate} />
+          </div>
+        </div>
+
+        {/* Submit button. */}
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Edit Exercise Log"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default EditExercise;
--- exercise-tracker/src/components/exercises-list.component.js
+++ exercise-tracker/src/components/exercises-list.component.js
@@ -1,10 +1,86 @@
-/*       Exercises list       */
+/*       Exercises list.       */
 
 // Setup.
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function Exercise(props) {
+  const { username, description, duration, date } = props.exercise;
+
+  return (
+    <tr>
+      <td>{username}</td>
+      <td>{description}</td>
+      <td>{duration}</td>
+      <td>{date.substring(0, 10)}</td>
+      <td>
+        <Link className="btn btn-primary" to={`/edit/${props.exercise._id}`}>
+          Edit
+        </Link>
+        <span> </span>
+        <button
+          className="btn btn-danger"
+          onClick={() => {
+            props.onDelete(props.exercise._id);
+          }}
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+}
 
 function ExercisesList() {
-  return <div>Exercises List</div>;
+  // Hooks.
+  const [exercises, setExercises] = useState([]);
+
+  // componentDidMount.
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/exercises/")
+      .then((res) => setExercises(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleDelete = (id) => {
+    axios
+      .delete(`http://localhost:5000/exercises/${id}`)
+      .then((res) =>
+        setExercises(exercises.filter((exercise) => exercise._id !== id))
+      )
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <h3>Logged Exercises</h3>
+      <table className="table">
+        <thead className="thead-light">
+          <tr>
+            <th>Username</th>
+            <th>Description</th>
+            <th>Duration</th>
+            <th>Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {exercises.map((exercise) => {
+            return (
+              <Exercise
+                key={exercise._id}
+                exercise={exercise}
+                onDelete={handleDelete}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default ExercisesList;
