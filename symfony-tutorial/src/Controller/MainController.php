6de1edd2551ed759ee11e487ea19ad81898006ab
Santiago Andrés Ardiles Páez <santiagoandres@outlook.de>
2021-01-05 13:23:05 +0100

    More Symfony and Twig.

--- symfony-tutorial/src/Controller/MainController.php
+++ symfony-tutorial/src/Controller/MainController.php
@@ -11,16 +11,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[ Route("/", name: "home")]
-    public function index()
-    {
-        return new Response(content: "<h1>hey folks!<h1>");
-    }
+  #[Route("/", name: "home")]
+  public function index()
+  {
+    return $this->render(view: "home/index.html.twig");
+  }
 
+  #[Route("/hello", name: "hello")]
+  public function hello()
+  {
+    return new Response(content: "<h1>Hello world!<h1>");
+  }
 
-    #[ Route("/custom/{name?}", name: "custom")]
-    public function custom(Request $request)
-    {
-        return new Response(content: "<h1>Welcome !</h1>");
-    }
+  #[Route("/custom/{name?}", name: "custom")]
+  public function custom(Request $request)
+  {
+    $name = $request->get(key: "name");
+    return $this->render("home/custom.html.twig", ["name" => $name,]);
+  }
 }
